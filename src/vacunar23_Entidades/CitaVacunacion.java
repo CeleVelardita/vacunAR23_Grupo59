f9de5c3957dafc38bde9b8514920c809f2da885d
FrankMortara <francomortara5@gmail.com>
2023-10-03 20:28:28 -0300

    citaVacunacion

--- src/vacunar23_Entidades/CitaVacunacion.java
+++ src/vacunar23_Entidades/CitaVacunacion.java
@@ -1,14 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vacunar23_Entidades;
 
-/**
- *
- * @author Frank
- */
+import java.sql.Time;
+import java.time.LocalDate;
+
+
 public class CitaVacunacion {
+  //Declaración de atributos
+  private int codCita;
+  private int idCiudadano;
+  private int codRefuerzo;
+  private LocalDate fechaHoraCita;
+  private String centroVacunacion;
+  private Time fechaHoraColoca;
+  private int idVacuna;
+  
+  //Constructores
+
+  public CitaVacunacion() {
+  }
+
+  public CitaVacunacion(int codCita, int idCiudadano, int codRefuerzo, LocalDate fechaHoraCita, String centroVacunacion, Time fechaHoraColoca, int idVacuna) {
+        this.codCita = codCita;
+        this.idCiudadano = idCiudadano;
+        this.codRefuerzo = codRefuerzo;
+        this.fechaHoraCita = fechaHoraCita;
+        this.centroVacunacion = centroVacunacion;
+        this.fechaHoraColoca = fechaHoraColoca;
+        this.idVacuna = idVacuna;
+    }
+  
+  //Getters y Setters
+
+    public int getCodCita() {
+        return codCita;
+    }
+
+    public void setCodCita(int codCita) {
+        this.codCita = codCita;
+    }
+
+    public int getIdCiudadano() {
+        return idCiudadano;
+    }
+
+    public void setIdCiudadano(int idCiudadano) {
+        this.idCiudadano = idCiudadano;
+    }
+
+    public int getCodRefuerzo() {
+        return codRefuerzo;
+    }
+
+    public void setCodRefuerzo(int codRefuerzo) {
+        this.codRefuerzo = codRefuerzo;
+    }
+
+    public LocalDate getFechaHoraCita() {
+        return fechaHoraCita;
+    }
+
+    public void setFechaHoraCita(LocalDate fechaHoraCita) {
+        this.fechaHoraCita = fechaHoraCita;
+    }
+
+    public String getCentroVacunacion() {
+        return centroVacunacion;
+    }
+
+    public void setCentroVacunacion(String centroVacunacion) {
+        this.centroVacunacion = centroVacunacion;
+    }
+
+    public Time getFechaHoraColoca() {
+        return fechaHoraColoca;
+    }
+
+    public void setFechaHoraColoca(Time fechaHoraColoca) {
+        this.fechaHoraColoca = fechaHoraColoca;
+    }
+
+    public int getIdVacuna() {
+        return idVacuna;
+    }
+
+    public void setIdVacuna(int idVacuna) {
+        this.idVacuna = idVacuna;
+    }
+  
     
+    //el toString
+
+    @Override
+    public String toString() {
+        return "CitaVacunacion{" + "codCita=" + this.codCita + ", idCiudadano=" + this.idCiudadano + ", codRefuerzo=" + this.codRefuerzo + ", fechaHoraCita=" + this.fechaHoraCita + ", centroVacunacion=" + this.centroVacunacion + ", fechaHoraColoca=" + this.fechaHoraColoca + ", idVacuna=" + this.idVacuna + '}';
+    }
+    
+    
+  
 }
