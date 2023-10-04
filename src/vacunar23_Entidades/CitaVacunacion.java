447c99cae1a007ff3561c2772f7117a73d0782c8
FrankMortara <francomortara5@gmail.com>
2023-10-04 19:24:03 -0300

    parte de entidad

--- src/vacunar23_Entidades/CitaVacunacion.java
+++ src/vacunar23_Entidades/CitaVacunacion.java
@@ -1,6 +1,83 @@
 
 package vacunar23_Entidades;
 
+import java.time.LocalDate;
+
 public class CitaVacunacion {
+    private int codCita;
+    private int idCiudadano;
+    private LocalDate fechaHoraCita;
+    private String centroVacunacion; 
+    private LocalDate fechaHoraColoca;
+    private int idVacuna;
+    //private Vacuna vacuna;
+    //private Ciudadano ciudadano;
+
+    public CitaVacunacion() {
+    }
+
+    public CitaVacunacion(int codCita, int idCiudadano, LocalDate fechaHoraCita, String centroVacunacion, LocalDate fechaHoraColoca, int idVacuna) {
+        this.codCita = codCita;
+        this.idCiudadano = idCiudadano;
+        this.fechaHoraCita = fechaHoraCita;
+        this.centroVacunacion = centroVacunacion;
+        this.fechaHoraColoca = fechaHoraColoca;
+        this.idVacuna = idVacuna;
+    }
+
+    public int getCodCita() {
+        return codCita;
+    }
+
+    public void setCodCita(int codCita) {
+        this.codCita = codCita;
+    }
+
+    public int getIdCiudadano() {
+        return idCiudadano;
+    }
+
+    public void setIdCiudadano(int idCiudadano) {
+        this.idCiudadano = idCiudadano;
+    }
+
+    public LocalDate getFechaHoraCita() {
+        return fechaHoraCita;
+    }
+
+    public void setFechaHoraCita(LocalDate fechaHoraCita) {
+        this.fechaHoraCita = fechaHoraCita;
+    }
+
+    public String getCentroVacunacion() {
+        return centroVacunacion;
+    }
+
+    public void setCentroVacunacion(String centroVacunacion) {
+        this.centroVacunacion = centroVacunacion;
+    }
+
+    public LocalDate getFechaHoraColoca() {
+        return fechaHoraColoca;
+    }
+
+    public void setFechaHoraColoca(LocalDate fechaHoraColoca) {
+        this.fechaHoraColoca = fechaHoraColoca;
+    }
+
+    public int getIdVacuna() {
+        return idVacuna;
+    }
+
+    public void setIdVacuna(int idVacuna) {
+        this.idVacuna = idVacuna;
+    }
+
+    @Override
+    public String toString() {
+        return "CitaVacunacion{" + "codCita=" + codCita + ", idCiudadano=" + idCiudadano + ", fechaHoraCita=" + fechaHoraCita + ", centroVacunacion=" + centroVacunacion + ", fechaHoraColoca=" + fechaHoraColoca + ", idVacuna=" + idVacuna + '}';
+    }
+    
+    
     
 }
--- src/vacunar23_Entidades/Laboratorio.java
+++ src/vacunar23_Entidades/Laboratorio.java
@@ -1,7 +1,69 @@
 
 package vacunar23_Entidades;
 
-
 public class Laboratorio {
+    private int idLaboratorio;
+    private int cuit;
+    private String nomLaboratorio;
+    private String pais; 
+    private String domComercial;
+
+    public Laboratorio() {
+    }
+
+    public Laboratorio(int idLaboratorio, int cuit, String nomLaboratorio, String pais, String domComercial) {
+        this.idLaboratorio = idLaboratorio;
+        this.cuit = cuit;
+        this.nomLaboratorio = nomLaboratorio;
+        this.pais = pais;
+        this.domComercial = domComercial;
+    }
+
+    public int getIdLaboratorio() {
+        return idLaboratorio;
+    }
+
+    public void setIdLaboratorio(int idLaboratorio) {
+        this.idLaboratorio = idLaboratorio;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(int cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getNomLaboratorio() {
+        return nomLaboratorio;
+    }
+
+    public void setNomLaboratorio(String nomLaboratorio) {
+        this.nomLaboratorio = nomLaboratorio;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getDomComercial() {
+        return domComercial;
+    }
+
+    public void setDomComercial(String domComercial) {
+        this.domComercial = domComercial;
+    }
+
+    @Override
+    public String toString() {
+        return "Laboratorio{" + "idLaboratorio=" + idLaboratorio + ", cuit=" + cuit + ", nomLaboratorio=" + nomLaboratorio + ", pais=" + pais + ", domComercial=" + domComercial + '}';
+    }
+    
+    
     
 }
