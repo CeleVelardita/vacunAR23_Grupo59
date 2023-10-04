1a20d9b097a9e90e01cc9893b371ed898f778503
FrankMortara <francomortara5@gmail.com>
2023-10-04 19:06:08 -0300

    nada

--- src/vacunar23_Entidades/Laboratorio.java
+++ src/vacunar23_Entidades/Laboratorio.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package vacunar23_Entidades;
 
-/**
- *
- * @author Frank
- */
+
 public class Laboratorio {
     
 }
